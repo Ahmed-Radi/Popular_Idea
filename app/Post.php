f95edc9f4dd5069d200082e227fd634774d696ef
Engy Ashraf <egy2962@gmail.com>
2020-03-22 15:59:40 -0700

    Add post page

---
+++ app/Post.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    //table name
+    protected $table = 'posts';
+    //primary key
+    public $primaryKey = 'id';
+    //timestamps
+    public $timestamps = true;
+
+    
+    
+}
