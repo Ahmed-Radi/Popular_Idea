629dfacfd7f5c063051605c0cc087d78db3d86e9
Ahmed-Radi <Ahmed-Radi@users.noreply.github.com>
2020-03-24 15:26:52 +0200

    Add files to Postpage Branch

---
+++ resources/views/inc/navbar.blade.php
@@ -0,0 +1,53 @@
+
+<nav class="navbar navbar-inverse">
+    <div class="container">
+        
+        
+        <a class="navbar-brand" href="{{ url('/') }}">
+            {{ config('app.name', 'Laravel') }}
+        </a>
+        
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <!-- Left Side Of Navbar -->
+            <ul class="nav navbar-nav">
+                &nbsp;
+            </ul>
+
+            <ul class="nav navbar-nav">
+              <li><a href="/">Home</a></li>
+              <li><a href="/posts">Blog</a></li>
+            </ul>
+            
+            <!-- Right Side Of Navbar -->
+            <ul class="nav navbar-nav navbar-right">
+                <!-- Authentication Links -->
+                @if (Auth::guest())
+                    <li><a href="{{ route('login') }}">Login</a></li>
+                    <li><a href="{{ route('register') }}">Register</a></li>
+                @else
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                            {{ Auth::user()->name }} <span class="caret"></span>
+                        </a>
+
+                        <ul class="dropdown-menu" role="menu">
+                            
+                            <li>
+                                <a href="{{ route('logout') }}"
+                                    onclick="event.preventDefault();
+                                              document.getElementById('logout-form').submit();">
+                                    Logout
+                                </a>
+
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                    {{ csrf_field() }}
+                                </form>
+                            </li>
+                        </ul>
+                    </li>
+                @endif
+            </ul>
+        </div>
+    </div>
+</nav>
---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+@section('content')
+<div class="jumbotron text-center">
+    <h1>Welcome To BlogApp!</h1>
+    <p>This is a kinda Blog application or some shit, not sure yet</p>
+    <p><a class="btn btn-primary btn-lg" href="/login" role="button">Login</a> <a class="btn btn-success btn-lg" href="/register" role="button">Register</a></p>
+</div>
+@endsection
