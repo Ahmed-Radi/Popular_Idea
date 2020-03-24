3f242b8aad8ef38f739a7009c34be1f7ecdaace4
yara-yehia <yehiayara116@gmail.com>
2020-03-25 01:25:30 +0200

    add create post

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -6,7 +6,7 @@
         @foreach ($posts as $post)
             <div class="well">
             <h3>    <a href="/posts/{{$post->id}}"> {{$post->title}} </a>    </h3>
-            <small>Written on </small>
+            <small>Written on {{$post->created_at}} by {{$post->user->name}}</small>
             </div>  
         @endforeach
         {{$posts->links()}}
