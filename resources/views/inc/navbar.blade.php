8c93f309dab9785cab6db9fc44f00869cd4ab65a
hagarhesham <61502963+hagarhesham@users.noreply.github.com>
2020-03-27 22:06:02 +0200

    modify Navbar

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -1,14 +1,22 @@
-
 <nav class="navbar navbar-inverse">
     <div class="container">
+        <div class="navbar-header">
 
+            <!-- Collapsed Hamburger -->
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
+                <span class="sr-only">Toggle Navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
 
-        <a class="navbar-brand" href="{{ url('/') }}">
-            {{ config('app.name', 'Laravel') }}
-        </a>
+            <!-- Branding Image -->
+            <a class="navbar-brand" href="{{ url('/') }}">
+                {{ config('app.name', 'Laravel') }}
+            </a>
+        </div>
 
-
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <div class="collapse navbar-collapse" id="app-navbar-collapse">
             <!-- Left Side Of Navbar -->
             <ul class="nav navbar-nav">
                 &nbsp;
@@ -16,6 +24,7 @@
 
             <ul class="nav navbar-nav">
               <li><a href="/">Home</a></li>
+              <li><a href="/posts/create">Create a Post</a></li>
               <li><a href="/posts">Blog</a></li>
             </ul>
 
@@ -32,7 +41,7 @@
                         </a>
 
                         <ul class="dropdown-menu" role="menu">
-
+                            
                             <li>
                                 <a href="{{ route('logout') }}"
                                     onclick="event.preventDefault();
