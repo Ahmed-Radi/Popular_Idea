ba53e22baacfecc94eccefcdd23b6efae64ab655
Ahmed-Radi <Ahmed-Radi@users.noreply.github.com>
2020-03-29 13:59:34 +0200

    Add feature to delete the post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -98,6 +98,19 @@ class PostController extends Controller
         return redirect('/posts')->with('success','Post Updated');
     }
 
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $post = Post::find($id);
+        $post->delete();
+        return redirect('/posts')->with('success','Post Removed');
 
+    }
+    
 
 }
